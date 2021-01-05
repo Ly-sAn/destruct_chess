1f6619e5bbdb8f6bcf42fea1129df1185edc67bf
Léna <lenapancher@gmail.com>
2021-01-05 18:08:34 +0100

    menu + commentaires

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class Menu {
 
-
-
     Scanner sc = new Scanner(System.in);
 
-    public void mainMenu(){
+    /**
+     * Function to display the main menu and select an option from this menu.
+     */
+    public void mainMenu() {
 
         boolean finished = false;
-
 
         System.out.println("============================");
         System.out.println("|     DESTRUCT CHEESE      |");
@@ -23,9 +23,7 @@ public class Menu {
         System.out.println("|  4. Exit                 |");
         System.out.println("============================");
 
-
-        while (!finished){
-
+        while (!finished) {
             String input = sc.next();
 
             switch (input) {
@@ -42,57 +40,88 @@ public class Menu {
                     menuScoreboard();
                 }
                 case "4" -> {
-                    System.out.println("Vous quittez le programme");
+                    System.out.println("You are leaving the programme.");
                     System.exit(0);
                 }
-                default -> System.out.println("Mauvais choix, veuillez le saisir à nouveau");
+                default -> System.out.println("Wrong choice, please enter it again.");
             }
         }
     }
 
+    /**
+     * Function to display the rules.
+     */
     public void menuRules() {
+
+        System.out.println("================================================================================");
+        System.out.println("|                                GAME RULES                                    |");
+        System.out.println("================================================================================");
+        System.out.println("|                           The aim of the game ?                              |");
+        System.out.println("| To destroy as many squares on the board as possible without getting trapped. |\n");
+        System.out.println("|                            What are the rules ?                              |");
+        System.out.println("| During his turn, a player can :                                              |");
+        System.out.println("| - move his pawn one square (vertically or horizontally)                      |");
+        System.out.println("| - then he destroys one square on the board.                                  |\n");
+        System.out.println("|                                How do I win ?                                |");
+        System.out.println("| The last player who can still move wins.                                     |");
+        System.out.println("================================================================================\n");
+
+        mainMenu();
+    }
+
+    /**
+     * Function that allows you to play the game.
+     */
+    public void menuGame() {
 
         boolean finished = false;
 
         System.out.println("============================");
-        System.out.println("|     DESTRUCT CHEESE      |");
-        System.out.println("============================");
-        System.out.println("| Options:                 |");
-        System.out.println("|  1. Game Rules           |");
-        System.out.println("|  2. Play                 |");
-        System.out.println("|  3. Scoreboard           |");
-        System.out.println("|  4. Back to Main Menu    |");
+        System.out.println("|           GAME           |");
         System.out.println("============================");
 
+        System.out.println("Type 'E' to exit the game.");
 
-        while (!finished){
+        while (!finished) {
             String input = sc.next();
 
-
-
-            switch (input){
-
-                case "4" -> {
-                    finished = true;
-                    mainMenu();
-                }
+            if ("E".equals(input)) {
+                finished = true;
+                mainMenu();
+            } else {
+                System.out.println("Wrong choice, please enter it again.");
             }
-
         }
 
-
-
-
+        // Sans doute ici qu'on va appeller la class Player, Display et Game.
     }
 
-    public void menuGame(){
+    /**
+     * Function to display the scores.
+     */
+    public void menuScoreboard() {
 
+        boolean finished = false;
 
+        System.out.println("============================");
+        System.out.println("|       SCORE BOARD        |");
+        System.out.println("============================");
+        System.out.println("| Highest scores :         |");
+        System.out.println("|                          |");
+        System.out.println("============================\n");
+        System.out.println("Type 'E' to exit the scoreboard.");
+
+        while (!finished) {
+            String input = sc.next();
+
+            if ("E".equals(input)) {
+                finished = true;
+                mainMenu();
+            } else {
+                System.out.println("Wrong choice, please enter it again.");
+            }
+        }
     }
 
-    public void menuScoreboard(){
-
-    }
-
-
+    // Ici va falloir appeller la class Score.
 }
